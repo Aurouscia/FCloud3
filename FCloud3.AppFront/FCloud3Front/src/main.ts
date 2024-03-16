299e41a2733c268a7b0180c620704094e87ec4ab
Au <18971501210@189.cn>
2024-03-16 22:05:02 +0800

    解决404文件大小写问题

--- FCloud3.AppFront/FCloud3Front/src/main.ts
+++ FCloud3.AppFront/FCloud3Front/src/main.ts
@@ -4,7 +4,7 @@ import './wikiViewStyles.scss'
 import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router';
 import { addIdentities } from './pages/Identities/routes'
-import NotFound from './pages/NotFound.vue';
+import NotFound from './pages/NotFoundPage.vue';
 import { addHomePage } from './pages/Home/routes';
 import { addWiki } from './pages/Wiki/routes';
 import { addTextSection } from './pages/TextSection/routes';
