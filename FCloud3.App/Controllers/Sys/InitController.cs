275242810f8922b8163f6e2f76fe9baa281bce6b
Au <18971501210@189.cn>
2024-05-11 18:33:16 +0800

    版主可批量自动添加“词条标题包含”

--- FCloud3.App/Controllers/Sys/InitController.cs
+++ FCloud3.App/Controllers/Sys/InitController.cs
@@ -5,20 +5,36 @@ using FCloud3.Entities.Files;
 using FCloud3.Entities.Identities;
 using FCloud3.Entities.Wiki;
 using FCloud3.Services.Etc.TempData.Context;
+using FCloud3.Services.Wiki;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace FCloud3.App.Controllers.Sys
 {
+    [Route("Init/{code}/[action]")]
     public class InitController:Controller
     {
         private readonly FCloudContext _context;
         private readonly TempDataContext _tempDataContext;
-        private readonly static Object migrateLockObj = new();
-        public InitController(FCloudContext context, TempDataContext tempDataContext)
+        private readonly WikiTitleContainService _wikiTitleContainService;
+        private readonly static object migrateLockObj = new();
+        public InitController(
+            FCloudContext context,
+            TempDataContext tempDataContext,
+            WikiTitleContainService wikiTitleContainService,
+            IConfiguration config,
+            IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
             _tempDataContext = tempDataContext;
+            _wikiTitleContainService = wikiTitleContainService;
+
+            var code = httpContextAccessor.HttpContext?.Request.RouteValues["code"]?.ToString();
+            if (string.IsNullOrWhiteSpace(code))
+                throw new Exception("请输入配置文件中的MasterAdminCode");
+            var trueCode = config["MasterAdminCode"] ?? throw new Exception("配置文件中MasterAdminCode未填");
+            if (trueCode != code)
+                throw new Exception("MasterAdminCode错误");
         }
         public IActionResult InitDb()
         {
@@ -26,7 +42,7 @@ namespace FCloud3.App.Controllers.Sys
             {
                 _context.Database.Migrate();
                 _tempDataContext.Database.Migrate();
-                return Ok("已Migrate成功");
+                return this.ApiResp("已Migrate成功", true);
             }
         }
         public IActionResult InitWikis()
@@ -117,50 +133,66 @@ namespace FCloud3.App.Controllers.Sys
                 {
                     Name = "一号文件夹",
                 };
-                _context.FileDirs.Add(d1);
-                _context.SaveChanges();
                 FileDir d2 = new FileDir()
                 {
                     Name = "二号文件夹",
                 };
-                _context.FileDirs.Add(d2);
-                _context.SaveChanges();
-
-                FileDir d3 = new FileDir()
-                {
-                    Name = "三号文件夹",
-                    ParentDir = d1.Id,
-                    Depth = 1
-                };
-                _context.FileDirs.Add(d3);
-                _context.SaveChanges();
-                FileDir d4 = new FileDir()
-                {
-                    Name = "四号文件夹",
-                    ParentDir = d1.Id,
-                    Depth = 1
-                };
-                _context.FileDirs.Add(d4);
-                _context.SaveChanges();
-
-                FileDir d5 = new FileDir()
-                {
-                    Name = "五号文件夹",
-                    ParentDir = d3.Id,
-                    Depth = 2
-                };
-                _context.FileDirs.Add(d5);
-                _context.SaveChanges();
-                FileDir d6 = new FileDir()
-                {
-                    Name = "六号文件夹",
-                    ParentDir = d4.Id,
-                    Depth = 2
-                };
-                _context.FileDirs.Add(d6);
+                _context.FileDirs.AddRange(d1, d2);
                 _context.SaveChanges();
             }
             return this.ApiResp();
+        }
+
+        /// <summary>
+        /// 【⚠警告】该操作对内存和数据库均有极大负担，请谨慎操作
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult WikiTitleContainAutoFill()
+        {
+            var existingContainsForText = _context.WikiTitleContains
+                .Where(x => x.Type == WikiTitleContainType.TextSection)
+                .Select(x => x.ObjectId);
+            var existingContainsForTable = _context.WikiTitleContains
+                .Where(x => x.Type == WikiTitleContainType.FreeTable)
+                .Select(x => x.ObjectId);
+
+            var allTexts = _context.TextSections
+                .Where(x => !x.Deleted)
+                .Where(x => !existingContainsForText.Contains(x.Id))
+                .Select(x => new { x.Id, x.Content }).ToList();
+            var allTables = _context.FreeTables
+                .Where(x => !x.Deleted)
+                .Where(x => !existingContainsForTable.Contains(x.Id))
+                .Select(x => new { x.Id, x.Data }).ToList();
+
+            List<WikiTitleContain> newContains = [];
+            allTexts.ForEach(x =>
+            {
+                var res = _wikiTitleContainService.AutoFill(x.Id, WikiTitleContainType.TextSection, x.Content ?? "");
+                res.Items.ForEach(r => {
+                    newContains.Add(new()
+                    {
+                        ObjectId = x.Id,
+                        Type = WikiTitleContainType.TextSection,
+                        WikiId = r.Id
+                    });
+                });
+            });
+            allTables.ForEach(x =>
+            {
+                var res = _wikiTitleContainService.AutoFill(x.Id, WikiTitleContainType.FreeTable, x.Data ?? "");
+                res.Items.ForEach(r => {
+                    newContains.Add(new()
+                    {
+                        ObjectId = x.Id,
+                        Type = WikiTitleContainType.FreeTable,
+                        WikiId = r.Id
+                    });
+                });
+            });
+            _context.WikiTitleContains.AddRange(newContains);
+            _context.SaveChanges();
+            return this.ApiResp("已完成", true);
         }
     }
 }
