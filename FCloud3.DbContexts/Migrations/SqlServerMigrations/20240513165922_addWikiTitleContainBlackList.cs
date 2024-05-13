3636ef34de300c7233f961446aa77e5cce864ef4
Au <18971501210@189.cn>
2024-05-14 01:05:45 +0800

    为sqlserver模型更新“标题包含”黑名单字段

---
+++ FCloud3.DbContexts/Migrations/SqlServerMigrations/20240513165922_addWikiTitleContainBlackList.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCloud3.DbContexts.Migrations.SqlServerMigrations
+{
+    /// <inheritdoc />
+    public partial class addWikiTitleContainBlackList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "BlackListed",
+                table: "WikiTitleContains",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BlackListed",
+                table: "WikiTitleContains");
+        }
+    }
+}
