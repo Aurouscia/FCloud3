a22827b9f638d2ab35c77aaf261edc4ca485e45e
Au <18971501210@189.cn>
2024-06-13 01:06:01 +0800

    添加词条隐藏属性的sqlserver迁移

---
+++ FCloud3.DbContexts/Migrations/SqlServerMigrations/20240612170420_addWikiItemSealed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCloud3.DbContexts.Migrations.SqlServerMigrations
+{
+    /// <inheritdoc />
+    public partial class addWikiItemSealed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Sealed",
+                table: "WikiItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sealed",
+                table: "WikiItems");
+        }
+    }
+}
