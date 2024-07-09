1d859f58a3f974f680a63b4a43101bfc99073568
Au <18971501210@189.cn>
2024-07-09 21:34:52 +0800

    优化词条目录显示

--- FCloud3.AppFront/FCloud3Front/src/components/Wiki/TitleTree.vue
+++ FCloud3.AppFront/FCloud3Front/src/components/Wiki/TitleTree.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { ParserTitleTreeNode } from '@/models/wikiParsing/wikiParsingResult';
-import { isDefaultFolded } from '@/utils/wikiView/titleClickFold';
+import { isDefaultFolded, removeDefaultFoldedMark } from '@/utils/wikiView/titleClickFold';
 import { cmtTitleId } from '@/models/messages/comment';
 
 const props = defineProps<{
@@ -46,10 +46,10 @@ defineExpose({
 <template>
 <div class="titleTreeNodes" :class="{master:props.isMaster}">
     <div v-for="t in props.titleTree" :key="t.Id">
-        <div v-if="t.Text && !isDefaultFolded(t.Text)" class="titleTreeNodeText" :class="{master:props.isMaster}" 
+        <div v-if="t.Text && (!isDefaultFolded(t.Text) || t.Level===0)" class="titleTreeNodeText" :class="{master:props.isMaster}" 
             @click="emit('clickTitle',t.Id)" :id="elementId(t.Id)">
             <div class="currentMark"></div>
-            <span v-html="t.Text"></span>
+            <span v-html="removeDefaultFoldedMark(t.Text)"></span>
         </div>
         <TitleTree v-if="t.Subs" :titleTree="t.Subs" @clickTitle="id=>emit('clickTitle',id)" ref="subs"></TitleTree>
     </div>
