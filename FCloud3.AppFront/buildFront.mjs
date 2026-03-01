31cc2a38b0088534b00d6eca9ae4690f46f304de
Au <18971501210@189.cn>
2026-03-01 23:29:10 +0800

    build: 修改buildFront.mjs脚本

--- FCloud3.AppFront/buildFront.mjs
+++ FCloud3.AppFront/buildFront.mjs
@@ -6,5 +6,5 @@ const mainDir = './FCloud3Front'
 console.log('=========开始编译插件=========')
 execSync(`node buildPlugins.mjs`, { cwd: pluginsDir, stdio: 'inherit' })
 console.log('=========开始编译前端主程序=========')
-execSync('npm i', { cwd: mainDir, stdio: 'inherit' })
+execSync('pnpm install', { cwd: mainDir, stdio: 'inherit' })
 execSync('npm run build', { cwd: mainDir, stdio: 'inherit' })
