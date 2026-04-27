f5aa6fc90ec8f77db6300ea0c3068c135fcb6edd
Au <18971501210@189.cn>
2026-04-27 22:56:23 +0800

    chore: 添加前端开发环境proxy设置

--- FCloud3.AppFront/FCloud3Front/vite.config.ts
+++ FCloud3.AppFront/FCloud3Front/vite.config.ts
@@ -1,47 +1,50 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { appVersionMark, ViteAppVersionConfig } from '@aurouscia/vite-app-version'
 const versionConfig:ViteAppVersionConfig = {filePath: 'feVersion.txt'}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    appVersionMark(versionConfig),
-  ],
-  define: {
-    __VERSION_CONFIG__: `'${JSON.stringify(versionConfig)}'`
-  },
-  server: {
-    port: 5173
-  },
-  build: {
-    outDir: "../../FCloud3.App/wwwroot",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('@aurouscia'))
-            return 'au'
-          if (id.includes('node_modules'))
-            return 'libs'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, 'env', 'VITE_')
+
+  return {
+    plugins: [
+      vue(),
+      appVersionMark(versionConfig),
+    ],
+    define: {
+      __VERSION_CONFIG__: `'${JSON.stringify(versionConfig)}'`
+    },
+    server: {
+      port: 5173,
+      proxy:{
+        '/api':{
+          target: env.VITE_ProxyTarget,
+          changeOrigin: true
         }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '~': resolve('')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler'
-      },
     },
-  },
-  envDir: "env"
+    build: {
+      outDir: "../../FCloud3.App/wwwroot",
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('@aurouscia'))
+              return 'au'
+            if (id.includes('node_modules'))
+              return 'libs'
+          }
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+        '~': resolve('')
+      }
+    },
+    envDir: "env"
+  }
 })
