e75d36362cd09836d9f016f36b338da29fab6a28
Au <18971501210@189.cn>
2026-05-01 01:51:06 +0800

    fix: 修复fickit/article长篇小说无法滚回顶部的问题

--- FCloud3.AppFront/FCloud3Front/src/pages/FicKit/Article.vue
+++ FCloud3.AppFront/FCloud3Front/src/pages/FicKit/Article.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { injectHttp } from '@/provides';
 import { useIdentityInfoStore } from '@/utils/globalStores/identityInfo';
+import { useMainDivDisplayStore } from '@/utils/globalStores/mainDivDisplay';
 import { HomeView, useArticleApiStore } from '@fickit/article';
 import { storeToRefs } from 'pinia';
+import { onMounted, onUnmounted, useTemplateRef } from 'vue';
 
 const { iden } = storeToRefs(useIdentityInfoStore())
 const http = injectHttp()
@@ -11,18 +13,39 @@ useArticleApiStore().setRequestInitProvider(()=>{
         headers: {Authorization: `Bearer ${http.jwtToken}`}
     }
 })
+
+const articleOuter = useTemplateRef('articleOuter')
+
+const { restrictContentMaxWidth } = storeToRefs(useMainDivDisplayStore())
+onMounted(()=>{
+    restrictContentMaxWidth.value = false
+})
+onUnmounted(()=>{
+    restrictContentMaxWidth.value = true
+})
 </script>
 
 <template>
-<div class="fickit-article">
-<HomeView :user-id="iden.Id" :user-level="iden.Type"></HomeView>
+<div class="fickit-article" ref="articleOuter">
+    <HomeView :user-id="iden.Id" :user-level="iden.Type" :scroll-container="articleOuter"></HomeView>
 </div>
 </template>
 
 <style scoped lang="scss">
+@use '../../styles/globalValues';
+
 .fickit-article{
-    max-width: 960px;
-    margin: 20px auto;
+    box-sizing: border-box;
+    max-width: 100vw;
+    padding: 30px 30px;
+    margin: 0px auto;
+    height: globalValues.$body-height;
+    overflow-y: auto;
+}
+@media screen and (min-width: 1300px) {
+    .fickit-article{
+        padding: 30px 200px;
+    }
 }
 :deep(h1){
     border: none;
