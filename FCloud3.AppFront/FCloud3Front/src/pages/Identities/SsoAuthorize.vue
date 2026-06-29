02492d46229a4baa4b5693c9e71727c2aef2bbe6
Au <18971501210@189.cn>
2026-06-29 23:25:52 +0800

    fix(sso): 修复ssoAuthorize页面中的json大小写问题

--- FCloud3.AppFront/FCloud3Front/src/pages/Identities/SsoAuthorize.vue
+++ FCloud3.AppFront/FCloud3Front/src/pages/Identities/SsoAuthorize.vue
@@ -9,31 +9,31 @@ import { useIdentityRoutesJump } from './routes/routesJump';
 import { recoverTitle, setTitleTo } from '@/utils/titleSetter';
 
 export interface F3SsoIssuerAudienceOptions {
-    Id: string;
-    DisplayName: string;
-    Origin: string;
-    Avatar: string;
-    RequireLevel: number;
+    id: string;
+    displayName: string;
+    origin: string;
+    avatar: string;
+    requireLevel: number;
 }
 
 export interface F3SsoIssuerOptions {
-    Id: string;
-    Enabled: boolean;
-    Audiences: F3SsoIssuerAudienceOptions[];
+    id: string;
+    enabled: boolean;
+    audiences: F3SsoIssuerAudienceOptions[];
 }
 
 export interface F3SsoAudienceIssuerOptions {
-    Id: string;
-    DisplayName: string;
-    Origin: string;
-    Avatar: string;
-    ClientId: string;
+    id: string;
+    displayName: string;
+    origin: string;
+    avatar: string;
+    clientId: string;
 }
 
 export interface F3SsoAudienceOptions {
-    Id: string;
-    Enabled: boolean;
-    Issuers: F3SsoAudienceIssuerOptions[];
+    id: string;
+    enabled: boolean;
+    issuers: F3SsoAudienceIssuerOptions[];
 }
 
 export interface F3SsoCodeResponse {
@@ -41,7 +41,7 @@ export interface F3SsoCodeResponse {
 }
 
 async function getF3SsoConfig(http: HttpClient): Promise<F3SsoIssuerOptions | undefined> {
-    return await http.requestRaw<F3SsoIssuerOptions>('/f3sso/iss/config', undefined, ['Id', 'Enabled', 'Audiences']);
+    return await http.requestRaw<F3SsoIssuerOptions>('/f3sso/iss/config', undefined, ['id', 'enabled', 'audiences']);
 }
 
 async function requestF3SsoCode(http: HttpClient, clientId: string): Promise<string | undefined> {
@@ -58,14 +58,14 @@ const loaded = ref<boolean>(false);
 const config = ref<F3SsoIssuerOptions>();
 const clientId = route.query.clientId?.toString() ?? '';
 const selectedAudience = computed<F3SsoIssuerAudienceOptions | undefined>(() => {
-    return config.value?.Audiences.find(c => c.Id === clientId);
+    return config.value?.audiences.find(c => c.id === clientId);
 });
 const levelSufficient = computed<boolean>(() => {
-    return iden.value.Type >= (selectedAudience.value?.RequireLevel ?? 255);
+    return iden.value.Type >= (selectedAudience.value?.requireLevel ?? 255);
 });
 
 async function authorize() {
-    if (!config.value?.Id) {
+    if (!config.value?.id) {
         return;
     }
     if (!levelSufficient.value) {
@@ -73,9 +73,9 @@ async function authorize() {
         return;
     }
     const code = await requestF3SsoCode(httpClient, clientId);
-    if (code && selectedAudience.value?.Origin) {
-        const origin = selectedAudience.value.Origin.replace(/\/$/, '');
-        const issuerId = encodeURIComponent(config.value.Id);
+    if (code && selectedAudience.value?.origin) {
+        const origin = selectedAudience.value.origin.replace(/\/$/, '');
+        const issuerId = encodeURIComponent(config.value.id);
         window.location.href = `${origin}/f3sso/aud/validate?code=${encodeURIComponent(code)}&issuerId=${issuerId}`;
     }
 }
@@ -100,7 +100,7 @@ onUnmounted(() => {
             <div>请先登录本站账号</div>
             <button @click="jumpToLogin(route.fullPath)">去登录</button>
         </div>
-        <div v-else-if="!config?.Enabled" class="centerBox">
+        <div v-else-if="!config?.enabled" class="centerBox">
             SSO 服务未启用
         </div>
         <div v-else-if="!selectedAudience" class="centerBox">
@@ -110,8 +110,8 @@ onUnmounted(() => {
             该账号等级不足以登录目标应用
         </div>
         <div v-else class="centerBox">
-            <img v-if="selectedAudience.Avatar" :src="selectedAudience.Avatar" alt="应用图标" class="avatar"/>
-            <div>确认授权{{ selectedAudience.DisplayName }}登录吗？</div>
+            <img v-if="selectedAudience.avatar" :src="selectedAudience.avatar" alt="应用图标" class="avatar"/>
+            <div>确认授权{{ selectedAudience.displayName }}登录吗？</div>
             <button @click="authorize">确认授权</button>
         </div>
     </div>
